ff3ab24c57c2514f73fc602833e654ebd3933370
Charly2408 <charly240894@gmail.com>
2015-12-13 15:38:38 -0700

    [fix]Se terminó el diseo de contacto y sus respectivas validaciones. Los script de las bases de datos están terminados. El diseño de la pantalla nosotros se ha terminado. La funcionalidad en la página de info de peliculas está completa. La funcionalidad en la página de contacto fue terminada.[fixes #4][fixes #5][fixes #6][fixes #10][fixes #19][fixes #20].

--- datos/comentarios.php
+++ datos/comentarios.php
@@ -1,188 +1,62 @@
-<?php 
-	$server="localhost";
-	$user="root";
-	$password="";
-	$bd="comentarios";
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ 	<script src="../js/bootstrap.js"></script>
+ 	<title>Respuesta</title>
+ </head>
+ <body>
+ 	<?php 
+	 	$server="localhost";
+	 	$user="root";
+	 	$password="";
+	 	$bd="comentarios";
 
-	$conexion=new mysqli($server, $user, $password, $bd);
+	 	$conexion=new mysqli($server, $user, $password, $bd);
 
-	if ($conexion->connect_error) {
-		print("Error en la conexión a la Base de Datos.");
-	}
+	 	if ($conexion->connect_error) {
+	 		print("Error en la conexión a la Base de Datos.");
+	 	}
 
-	$nombre=$_POST["nombre"];
-	$apellido=$_POST["apellido"];
-	$sucursal=$_POST["sucursal"];
-	$asunto=$_POST["asunto"];
-	$comentario=$_POST["comentario"];
+	 	$nombre=$_POST["nombre"];
+	 	$apellido=$_POST["apellido"];
+	 	$sucursal=$_POST["sucursal"];
+	 	$asunto=$_POST["asunto"];
+	 	$comentario=$_POST["comentario"];
 
-	$sql=mysqli_query($conexion, "INSERT INTO comentarios (nombre, apellido, sucursal, asunto, comentario) values ('$nombre', '$apellido', $sucursal, '$asunto', '$comentario')");
-?>
+	 	$sql=mysqli_query($conexion, "INSERT INTO comentarios (nombre, apellido, sucursal, asunto, comentario) values ('$nombre', '$apellido', $sucursal, '$asunto', '$comentario')");
+ 	?>
+ 	<div id="myModal" class="modal fade" role="dialog" data-backdrop="static" data-keyboard="false">
+ 		<div class="modal-dialog" >
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Cinesuyu</title>
-		<meta charset="UTF-8">
-		<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-		<link rel="stylesheet" type="text/css" href="../css/estilos_navbar_footer.css">
-		<link rel="stylesheet" type="text/css" href="../css/estilos_contacto.css">
-		<link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
-		<script src="../js/validaContacto.js"></script>
-	</head>
-	<body>
-		<div class="container">
-			<div class="row">
-				<nav class="navbar">
-					<div class="col-xs-12 col-sm-12 col-md-2 col-lg-2"> 
-						<a href="index.html">
-							<img class="img-responsive imagen-centrada logo" src="../img/logo.png" alt="Logo empresa">
-						</a>
-					</div>
-					<div class="col-xs-12 col-sm-12 col-md-10 col-lg-10">      	
-						<form class="navbar-form form-menu" role="search">
-							<div class="form-group">
-								<select class="form-control combo-menu">
-									<option value="">Seleccione una ciudad</option>
-									<option value="">Culiacán</option>
-									<option value="">Mazatlán</option>
-									<option value="">Los Mochis</option>
-								</select>	
-								<select class="form-control combo-menu">
-									<option value="">Seleccione un cine</option>
-									<option value="">Cine 1</option>
-									<option value="">Cine 2</option>
-									<option value="">Cine 3</option>
-								</select>	
-							</div>
-							<button type="submit" class="btn btn-default boton-menu btn btn-danger">Ver cartelera</button>
-						</form>
-					</div>	
-				</nav>
-			</div>
-			<div class="row text-center">
-				<div class="col-xs-12">
-					<h1 class="h1-contacto">Contáctanos</h1>
-					<h4 class="h4-contacto">Déjanos tus comentarios, queremos saber lo que piensas</h4>
-					<?php
-						if ($sql) {
-					?>		
-							<h4 style="color: green" class="h4-contacto">Comentarios enviados con éxito</h4>
-					<?php
-						} else	{
-					?>		
-							<h4 style="color: reed" class="h4-contacto">Error al enviar comentarios.</h4>
-					<?php	
-						}	
-							$conexion->close();
-					?>
-				</div>
-				<div class="col-xs-4 col-xs-offset-4 col-md-2 col-md-offset-5 separa-hruler">
-					<hr>
-				</div>
-			</div>
-			<div class="row separa-form">
-				<form method="post" action="comentarios.php" name="formContacto" onsubmit="return validarContacto()">
-					<div class="form-group col-xs-8 col-md-4 col-xs-offset-2">
-						<label for="nombre">Nombre(s):* </label>
-						<input id="nombre" name="nombre" type="text" class="form-control" placeholder="Teclea tu nombre">
-					</div>
-					<div class="form-group col-xs-8 col-md-4 col-xs-offset-2 col-md-offset-0">
-						<label for="apellido">Apellido(s):* </label>
-						<input id="apellido" name="apellido" type="text" class="form-control" placeholder="Teclea tu apellido">
-					</div>
-					<div class="col-xs-8 col-xs-offset-2">
-						<div class="form-group">
-							<label for="sucursal">Para sucursal:* </label>
-							<select name="sucursal" id="sucursal" class="form-control">
-								<option value="">Seleccione una sucursal</option>
-								<option value="1">CineSuyu - Zapata</option>
-								<option value="2">CineSuyu - La Lomita</option>
-								<option value="3">CineSuyu - La Playita</option>
-								<option value="4">CineSuyu - Pescado</option>
-								<option value="5">CineSuyu - Del Olmo</option>
-								<option value="6">CineSuyu - Santa Rosa</option>
-							</select>
-						</div>
-						<div class="form-group">
-							<label for="asunto">Asunto:* </label>
-							<input id="asunto" name="asunto" type="text" class="form-control" placeholder="Teclea tu asunto">
-						</div>
-						<div class="form-group">
-							<label for="comentario">Comentarios:* </label>
-							<textarea name="comentario" id="comentario" cols="30" rows="7" placeholder="Deja tus comentarios" class="form-control"></textarea>
-						</div>
-						<div class="form-group">
-							<input type="submit" value="Enviar mensaje" class="btn btn-success btn-lg btn-enviar-letra">
-						</div>
-					</div>
-				</form>	
-			</div>
-			<div class="row row-footer">
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 div-footer">
-					<h3 class="h3-footer">Acerca de nosotros</h3>
-					<a class="a-footer" href="http://www.w3schools.com">Nosotros</a>
-					<a class="a-footer" href="http://www.w3schools.com">Términos y condiciones</a>
-				</div>
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 div-footer">
-					<h3 class="h3-footer">Contacto</h3>
-					<a class="a-footer" href="http://www.w3schools.com">Dejanos tus comentarios</a>
-					<a class="a-footer">01 800 56 56 56</a>
-				</div>	
-				<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 div-footer">
-					<h3 class="h3-footer">Redes sociales</h3>
-					<div class="col-lg-2 icon-redes-footer">
-						<a href="http://www.facebook.com" target="_blank">
-							<img class="img-responsive" src="../img/icons/facebook.png" 
-							onmouseover="this.src='../img/icons/facebook_hover.png'"
-							onmouseout="this.src='../img/icons/facebook.png'"alt="Logo empresa">
-						</a>
-					</div>
-					<div class="col-lg-2 icon-redes-footer">
-						<a href="http://www.twitter.com" target="_blank">
-							<img class="img-responsive" src="../img/icons/twitter.png" alt="Logo empresa"
-							onmouseover="this.src='../img/icons/twitter_hover.png'"
-							onmouseout="this.src='../img/icons/twitter.png'"alt="Logo empresa" >
-						</a>
-					</div>
-					<div class="col-lg-2 icon-redes-footer">
-						<a href="http://www.plus.google.com" target="_blank">
-							<img class="img-responsive" src="../img/icons/google.png" alt="Logo empresa" 
-							onmouseover="this.src='../img/icons/google_hover.png'"
-							onmouseout="this.src='../img/icons/google.png'" alt="Logo empresa">
-						</a>	
-					</div>
-					<div class="col-lg-2 icon-redes-footer">
-						<a href="http://www.instagram.com" target="_blank">
-							<img class="img-responsive" src="../img/icons/instagram.png" alt="Logo empresa"
-							onmouseover="this.src='../img/icons/instagram_hover.png'"
-							onmouseout="this.src='../img/icons/instagram.png'"alt="Logo empresa">
-						</a>
-					</div>
-					<div class="col-lg-2 icon-redes-footer">
-						<a href="http://www.youtube.com" target="_blank">
-							<img class="img-responsive" src="../img/icons/youtube.png" alt="Logo empresa"
-							onmouseover="this.src='../img/icons/youtube_hover.png'"
-							onmouseout="this.src='../img/icons/youtube.png'"alt="Logo empresa">
-						</a>
-					</div>
-				</div>		
-			</div>
-			<div class="footer">
-				<div class="container">
-					<p class="texto-footer"><small>©Copyrigth 2015. Todos los derechos reservados a Cinesuyu®</small></p>
-				</div>	
-			</div>	
-		</div>	
-	</body>
-</html>
-
-<?php
-	if ($sql) {
-		print("Registro exitoso <br>");
-	} else	{
-		print("Error al registrar paciente <br>");
-	}
-	print("<a href='../contacto.php'>Regresar a la página anterior</a>");
-	$conexion->close();
- ?>
+ 			<!-- Modal content-->
+ 			<div class="modal-content">
+ 				<div class="modal-header">
+ 					<button type="button" class="close"><a style="text-decoration: none" href='../contacto.php'>&times;</a></button>
+ 					<h4 class="modal-title">Resultado del envío</h4>
+ 				</div>
+ 				<div class="modal-body">
+ 					<p>
+ 						<?php
+	 						if ($sql) {
+	 							print("El comentario fué enviado correctamente.");
+	 						} else	{
+	 							print("Error al enviar comentario.");
+	 						}
+	 						$conexion->close();
+ 						?>
+ 					</p>
+ 				</div>
+ 				<div class="modal-footer">
+ 					<button type="button" class="btn btn-primary" ><a style="text-decoration: none; color: #FFFFFF" href='../contacto.php'>Regresar</a></button>
+ 				</div>
+ 			</div>
+ 		</div>
+ 	</div>
+ 	<script>
+ 		$('#myModal').modal('show'); 
+ 	</script>
+ </body>
+ </html>
